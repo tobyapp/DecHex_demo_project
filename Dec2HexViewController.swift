eae38d5f901d91a1ce6308ebec0ce96dbb6b0af7
Toby Applegate <up661724@myport.ac.uk>
2015-11-10 19:35:58 +0000

    finished version before playing with watch kit

--- Dec2HexViewController.swift
+++ Dec2HexViewController.swift
@@ -12,6 +12,35 @@ class Dec2HexViewController: UIViewController {
 
     var brain = ConverterBrain()
     
+    @IBAction func warningmessage(sender: AnyObject) {
+                        let alertController = UIAlertController(
+                            title: "test title, hey that they both start with a 'T'",
+                            message: "shouldnt have pressed this button...",
+                            preferredStyle: .Alert)
+                        let cancelAction = UIAlertAction(
+                            title: "meh",
+                            style: UIAlertActionStyle.Destructive,
+                            handler: nil)
+                        let otherAction = UIAlertAction(
+                            title: "other",
+                            style: UIAlertActionStyle.Default,
+                            handler: {action in print("confirm was tapped")})
+                        alertController.addAction(cancelAction)
+        alertController.addAction(otherAction)
+        
+        let openAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
+            if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+                UIApplication.sharedApplication().openURL(url)
+            }
+        }
+        alertController.addAction(openAction)
+
+        
+                            self.presentViewController(alertController, animated: true, completion: nil)
+                    
+                            print("done")
+        
+    }
     @IBOutlet weak var answerDisplay: UILabel!
     @IBOutlet weak var decInput: UITextField!
     @IBAction func convert(sender: UIButton) {
@@ -29,6 +58,7 @@ class Dec2HexViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
+       
 
         // Do any additional setup after loading the view.
     }
